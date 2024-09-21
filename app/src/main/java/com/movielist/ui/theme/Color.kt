857a2ca421009e3c5a5b40536a9e2f6f3826cba5
Andreasberg89 <andber9@gmail.com>
2024-09-21 16:21:56 +0200

    Initial setup and colors
    Initial setup of project and color profile for the site.

---
+++ app/src/main/java/com/movielist/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.movielist.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple = Color(0xFFc38fff)
+val DarkPurple = Color(0xFF40344f)
+val LightBlack = Color(0xFF101010)
+val DarkGray = Color(0xFF1c1c1c)
+val Gray = Color(0xFF2c2c2c)
+val LightGray = Color(0xFF2c2c2c)
+val White = Color(0xFFffffff)
