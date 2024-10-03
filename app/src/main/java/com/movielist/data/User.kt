24d430f57cc777ab7fa4f369d8d0d57bb5137d33
M1kke95 <m1kke95@hotmail.com>
2024-10-03 22:02:57 +0200

    la til konstruktør i User
    User har fått createNewUser funksjon

--- app/src/main/java/com/movielist/data/User.kt
+++ app/src/main/java/com/movielist/data/User.kt
@@ -5,9 +5,41 @@ import java.util.UUID
 data class User (
     val id: UUID = UUID.randomUUID(),
     val userName: String,
+    val email: String,
+    val friendList: List<User> = emptyList(),
+    val myReviews: List<Review> = emptyList(),
+    val favoriteCollection: List<ListItem>,
     val profileImageID: Int,
     val completedShows: List<ListItem>,
     val wantToWatchShows: List<ListItem>,
     val droppedShows: List<ListItem>,
     val currentlyWatchingShows: List<ListItem>
 )
+
+fun createNewUser(
+    userName: String,
+    email: String,
+    friendList: List<User> = emptyList(),
+    myReviews: List<Review> = emptyList(),
+    favoriteCollection: List<ListItem> = emptyList(),
+    profileImageID: Int,
+    completedShows: List<ListItem> = emptyList(),
+    wantToWatchShows: List<ListItem> = emptyList(),
+    droppedShows: List<ListItem> = emptyList(),
+    currentlyWatchingShows: List<ListItem> = emptyList()
+): User {
+    return User(
+        userName = userName,
+        email = email,
+        friendList = friendList,
+        myReviews = myReviews, 
+        favoriteCollection = favoriteCollection,
+        profileImageID = profileImageID,
+        completedShows = completedShows,
+        wantToWatchShows = wantToWatchShows,
+        droppedShows = droppedShows,
+        currentlyWatchingShows = currentlyWatchingShows
+    )
+}
+
+
