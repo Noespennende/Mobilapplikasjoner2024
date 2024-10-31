692d29f2a03ba0bc2ce543c1fd6b17571027e8a3
M1kke95 <m1kke95@hotmail.com>
2024-10-31 21:24:35 +0100

    addproduction til user og hente ut shows
    funksjoner som gjør mye av det samme bare til forskjellige lister

--- app/src/main/java/com/movielist/controller/ControllerViewModel.kt
+++ app/src/main/java/com/movielist/controller/ControllerViewModel.kt
@@ -1,18 +1,28 @@
 package com.movielist.controller
 
+import android.content.Context
 import androidx.compose.runtime.State
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseUser
+import com.movielist.data.getUsersWatchingCollection
 import com.movielist.model.ListItem
+import com.movielist.model.Movie
+import com.movielist.model.Production
+import com.movielist.model.TVShow
 import com.movielist.model.User
 import com.movielist.viewmodel.AuthViewModel
+import com.movielist.viewmodel.QueryViewModel
 import com.movielist.viewmodel.UserViewModel
+
 import kotlinx.coroutines.flow.StateFlow
+import java.util.Calendar
+
 
 class ControllerViewModel (
     private val userViewModel: UserViewModel,
-    private val authViewModel: AuthViewModel
+    private val authViewModel: AuthViewModel,
+
 ) : ViewModel() {
 
     /* USER LOGIC */
@@ -35,5 +45,108 @@ class ControllerViewModel (
         authViewModel.checkUserStatus() // Kall autentiseringstatus
     }
 
+
+    fun getCurrentlyWatchingShows(): MutableList<ListItem>? {
+        return loggedInUser.value?.currentlyWatchingShows
+
+    }
+
+    fun getCompletedShows(): MutableList<ListItem>?{
+        return loggedInUser.value?.completedShows
+    }
+
+    fun getDroppedShows(): MutableList<ListItem>?{
+        return loggedInUser.value?.droppedShows
+        }
+
+    fun getWantToWatchList(): MutableList<ListItem>?{
+        return loggedInUser.value?.wantToWatchShows
+    }
+
+
+    fun addProductionToWantToWatchList(production: Production) {
+        val user = loggedInUser.value
+        if (user != null) {
+            val listItem = ListItem(
+                currentEpisode = if (production is TVShow) 0 else -1,
+                score = 0,
+                production = production,
+                lastUpdated = Calendar.getInstance()
+            )
+            user.wantToWatchShows.add(listItem)
+        } else {
+            println("User is not logged in.")
+        }
+    }
+
+
+    fun addProductionToCompletedShows(production: Production) {
+        val user = loggedInUser.value
+        if (user != null) {
+            val listItem = ListItem(
+                currentEpisode = if (production is TVShow) production.episodes.size else -1,
+                score = 0,
+                production = production,
+                lastUpdated = Calendar.getInstance()
+            )
+            user.completedShows.add(listItem)
+        } else {
+            println("User is not logged in.")
+        }
+    }
+
+
+    fun addProductionToCurrentlyWatchingShows(production: Production) {
+        val user = loggedInUser.value
+        if (user != null) {
+            val listItem = ListItem(
+                currentEpisode = if (production is TVShow) 1 else -1,
+                score = 0,
+                production = production,
+                lastUpdated = Calendar.getInstance()
+            )
+            user.currentlyWatchingShows.add(listItem)
+        } else {
+            println("User is not logged in.")
+        }
+    }
+
+
+    fun addProductionToDroppedShows(production: Production) {
+        val user = loggedInUser.value
+        if (user != null) {
+            val listItem = ListItem(
+                currentEpisode = if (production is TVShow) 0 else -1,
+                score = 0,
+                production = production,
+                lastUpdated = Calendar.getInstance()
+            )
+            user.droppedShows.add(listItem)
+        } else {
+            println("User is not logged in.")
+        }
+    }
+
+  
+    fun addProductionToFavoriteCollection(production: Production) {
+        val user = loggedInUser.value
+        if (user != null) {
+            val listItem = ListItem(
+                currentEpisode = if (production is TVShow) 0 else -1,
+                score = 0,
+                production = production,
+                lastUpdated = Calendar.getInstance()
+            )
+            user.favoriteCollection.add(listItem)
+        } else {
+            println("User is not logged in.")
+        }
+    }
 }
+
+
+
+
+}
+
 
